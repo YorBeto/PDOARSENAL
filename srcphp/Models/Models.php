b4769fe75c31c27b148b67a4a230a11190f43acd
DayTorres22 <yayaedithdelacruz@gmail.com>
2024-07-25 10:05:02 -0600

    pimer commit

--- srcphp/Models/Models.php
+++ srcphp/Models/Models.php
@@ -20,7 +20,7 @@
 
         public function __construct()
         {
-            $cc = new  Conexion("arsenal_gym", "localhost", "root", "");
+            $cc = new  Conexion("arsenal_gym", "localhost", "root", "1234");
             self::$pdo = $cc->getPDO();
         }
         public function create(array $obj)
