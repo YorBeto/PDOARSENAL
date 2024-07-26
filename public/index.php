127321cdd3b5e4c1a53a91998dc766b64ab96cc4
JonathanRios18 <sikra18@gmail.com>
2024-07-26 16:09:47 -0600

    Datos de POST para productos

--- public/index.php
+++ public/index.php
@@ -3,8 +3,8 @@
 namespace proyecto;
 require("../vendor/autoload.php");
 
-
 use proyecto\Controller\crearPersonaController;
+use proyecto\Controller\ProductosController;
 use proyecto\Models\User;
 use proyecto\Response\Failure;
 use proyecto\Response\Success;
@@ -13,31 +13,25 @@ use proyecto\Models\socios;
 use proyecto\Models\inbody_citas;
 use proyecto\Models\productos_servicios;
 
+Router::get('/prueba', [crearPersonaController::class, "prueba"]);
 
-
-
-Router::get('/prueba',[crearPersonaController::class,"prueba"]);
-
-Router::get('/clientes',[clientes::class,"mostrarclientes"]);
-Router::get('/socios',[socios::class,"mostrarsocios"]);
-Router::get('/citas',[inbody_citas::class,"mostrarcitas"]);
-Router::get('/productos',[productos_servicios::class,"mostrarproductos"]);
+Router::get('/clientes', [clientes::class, "mostrarclientes"]);
+Router::get('/socios', [socios::class, "mostrarsocios"]);
+Router::get('/citas', [inbody_citas::class, "mostrarcitas"]);
+Router::get('/productos', [productos_servicios::class, "mostrarproductos"]);
 
 Router::get('/crearpersona', [crearPersonaController::class, "crearPersona"]);
 Router::get('/usuario/buscar/$id', function ($id) {
-
-
-
-    $user= User::find($id);
-    if(!$user)
-    {
-        $r= new Failure(404,"no se encontro el usuario");
+    $user = User::find($id);
+    if (!$user) {
+        $r = new Failure(404, "no se encontró el usuario");
         return $r->Send();
     }
-   $r= new Success($user);
+    $r = new Success($user);
     return $r->Send();
-
-
 });
 Router::get('/respuesta', [crearPersonaController::class, "response"]);
+Router::post('/insertarproducto', [ProductosController::class, "insertarProducto"]);
 Router::any('/404', '../views/404.php');
+
+?>
