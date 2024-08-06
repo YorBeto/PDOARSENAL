3b2f9d6693db023173efb028ebfd3517159f4af9
JesusAlbertoTovar <tovar25072005@gmail.com>
2024-08-05 19:07:20 -0600

    cambios necesarios para el funcionamiento

--- srcphp/Models/Models.php
+++ srcphp/Models/Models.php
@@ -19,7 +19,7 @@
         {
 
 
-            $cc = new  Conexion("arsenal_gym", "3.142.149.148", "root", "1234");
+            $cc = new  Conexion("arsenal_gym", "localhost", "root", "");
 
             self::$pdo = $cc->getPDO();
         }
