f6ed9c20c85a2646b39b93282aa9cf2960a32a7f
JesusAlbertoTovar <tovar25072005@gmail.com>
2024-08-06 14:09:00 -0600

    cambio en el modelo de los productos

--- srcphp/Models/productos_servicios.php
+++ srcphp/Models/productos_servicios.php
@@ -15,9 +15,10 @@ class productos_servicios extends Models
 
     public function mostrarproductos(){
     $producto=new Table();
-    $todoslosproductos=$producto ->query("SELECT productos_servicios.ID_PRODUCTO,productos_servicios.NOMBRE,productos_servicios.DESCRIPCION,
-                                        productos_servicios.PRECIO,productos_servicios.STOCK,categoria_productos.NOMBRE AS CATEGORIA
-                                        FROM categoria_productos INNER JOIN productos_servicios ON categoria_productos.ID_CATEGORIA=productos_servicios.ID_CATEGORIA");
+    $todoslosproductos=$producto ->query("SELECT productos_servicios.ID_PRODUCTO, productos_servicios.NOMBRE, productos_servicios.DESCRIPCION,
+    productos_servicios.PRECIO, productos_servicios.STOCK, categoria_productos.NOMBRE AS CATEGORIA
+    FROM categoria_productos
+    INNER JOIN productos_servicios ON categoria_productos.ID_CATEGORIA = productos_servicios.ID_CATEGORIA");
 
     $success=new Success($todoslosproductos);
     return $success ->send();
