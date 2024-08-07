9c73ea537d278ce3c25b8baa526e182d008de4e9
JesusAlbertoTovar <tovar25072005@gmail.com>
2024-08-06 18:52:48 -0600

    pruebas al server 2

--- srcphp/Models/Models.php
+++ srcphp/Models/Models.php
@@ -17,7 +17,7 @@
         {
 
 
-            $cc = new  Conexion("arsenal_gym", "18.191.33.31", "root", "1234");
+            $cc = new  Conexion("arsenal_gym", "18.191.33.31", "root", "");
 
             self::$pdo = $cc->getPDO();
         }
